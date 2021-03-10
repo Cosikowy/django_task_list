95014ab2678b3f8b61418334b402cfe083f509e7
cosikowy <mr.cosik@gmail.com>
2021-03-10 16:01:46 +0100

    models prepared, user list done, back to proper git flow

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from apps.tasks.models import Task
+
+
+class UserListView(LoginRequiredMixin, ListView):
+    model = User
+    template_name = 'users/users_list.html'
+    context_object_name = 'users'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.all()
+        return context
+
+
+class UserProfileView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = 'users/profile.html'
+    context_object_name = 'tasks'*
+
+    # def get(self, request, *args, **kwargs):
+    # user = User.objects.filter(user=request.get)
+    # return super().get(request, *args, **kwargs)
+
 
 def register(request):
     if request.method == "POST":
@@ -9,18 +35,21 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created. Hello {username}. You can no log in.')
+            messages.success(
+                request, f'Account created. Hello {username}. You can no log in.')
             return redirect('login')
     else:
         form = UserRegisterForm()
-    
-    return render(request, 'users/register.html', { 'form':form })
+
+    return render(request, 'users/register.html', {'form': form})
+
 
 @login_required
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
@@ -29,12 +58,10 @@ def profile(request):
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
-    
 
     context = {
-        'u_form' : u_form,
-        'p_form' : p_form
+        'u_form': u_form,
+        'p_form': p_form
     }
 
-
-    return render(request, 'users/profile.html', context)
+    return render(request, 'users/profile-edit.html', context)
