adada20b2c8e0e6fd3219aa511af3e98a5d919b6
cosikowy <mr.cosik@gmail.com>
2021-03-11 04:58:13 +0100

    email sending logic and template

--- apps/tasks/tasks.py
+++ apps/tasks/tasks.py
@@ -1,13 +1,29 @@
 from celery import shared_task
-
-# celery tasks
-
-
-@shared_task
-def check_tasks():
-    pass
+from django.core.mail import send_mass_mail
+from .models import Task
+from django.template.loader import render_to_string
 
 
 @shared_task
-def send_enail():
-    pass
+def send_emails(*args, **kwargs):
+    send_mass_mail(*args, **kwargs)
+
+
+def check_tasks_daily():
+    tasks = Task.objects.all()
+    emails_to_send = []
+
+    for task in tasks:
+        if not task.status:
+            email_context = {'task': task}
+            email_template = render_to_string(
+                'tasks/email.html', context=email_context)
+            emails_to_send.append(
+                (
+                    task.title,
+                    email_template,
+                    None,
+                    [task.worker.email]
+                )
+            )
+    send_emails(tuple(emails_to_send), fail_silently=False)
